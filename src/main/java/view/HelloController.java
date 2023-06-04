e219235b93809675fd976e8c207b545a8661af33
Lior Hassin <liorhassin3@gmail.com>
2023-06-04 18:20:00 +0300

    General fixes/Implementations

--- src/main/java/view/HelloController.java
+++ src/main/java/view/HelloController.java
@@ -16,19 +16,22 @@ import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label nameLabel;
+    private Label nameLabelError;
     @FXML
     private TextField nameField;
     @FXML
-    private Label portLabel;
+    private Label portLabelError;
     @FXML
     private TextField portField;
+    @FXML
+    private Label ipLabelError;
+    @FXML
+    private TextField ipField;
 
     public void setNameLabel(ActionEvent event){
         String name = nameField.getText();
-        nameLabel.setText("Hello : " + name);
         String port = portField.getText();
-        portLabel.setText("Port : " + port);
+        String ip = ipField.getText();
     }
 
     @FXML
@@ -53,6 +56,54 @@ public class HelloController {
 
     @FXML
     public void StartAsHost(ActionEvent event) throws IOException{
+        boolean allValid = true;
+        if(!validName(nameField.getText())){
+            nameLabelError.setVisible(true);
+            allValid = false;
+        } else{
+            nameLabelError.setVisible(false);
+        }
+        if(!validPort(portField.getText())){
+            portLabelError.setVisible(true);
+            allValid = false;
+        } else{
+            portLabelError.setVisible(false);
+        }
+
+        if(allValid){
+            loadBoard(event);
+        }
+    }
+
+    @FXML
+    public void StartAsGuest(ActionEvent event) throws IOException{
+        boolean allValid = true;
+        if(!validName(nameField.getText())){
+            nameLabelError.setVisible(true);
+            allValid = false;
+        } else{
+            nameLabelError.setVisible(false);
+        }
+        if(!validPort(portField.getText())){
+            portLabelError.setVisible(true);
+            allValid = false;
+        } else{
+            portLabelError.setVisible(false);
+        }
+        if(!validIp(ipField.getText())){
+            ipLabelError.setVisible(true);
+            allValid = false;
+        } else{
+            ipLabelError.setVisible(false);
+        }
+
+        if(allValid){
+            loadBoard(event);
+        }
+    }
+
+    @FXML
+    public void loadHostForm(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("HostPage.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
@@ -62,7 +113,7 @@ public class HelloController {
     }
 
     @FXML
-    public void StartAsGuest(ActionEvent event) throws IOException{
+    public void loadGuestForm(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("GuestPage.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
@@ -72,7 +123,7 @@ public class HelloController {
     }
 
     @FXML
-    public void StartBoard(ActionEvent event) throws IOException{
+    public void loadBoard(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("BoardPage.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
@@ -85,5 +136,16 @@ public class HelloController {
         Platform.exit();
     }
 
+    public boolean validPort(String port){
+        return port.matches("(1000[1-9]|100[1-9]\\d|10[1-9]\\d{2}|1[1-9]\\d{3}|19999)");
+    }
+
+    public boolean validIp(String ip){
+        return ip.matches("(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}");
+    }
+
+    public boolean validName(String name){
+        return name.matches("^[A-Za-z]+$");
+    }
 
 }
